e0e385ecc3b83993de953a4c52f9653dc41f2faf
hongggweiii <103654486+13ack2basixs@users.noreply.github.com>
2026-03-21 00:24:12 +0800

    Initialise trades database schema

---
+++ scripts.sql
@@ -0,0 +1,12 @@
+-- Create trades table
+CREATE TABLE IF NOT EXISTS trades (
+    event_type String NOT NULL,
+    event_time Int64 NOT NULL,
+    symbol LowCardinality(String) NOT NULL,
+    trade_id Int64 NOT NULL,
+    price Decimal(18, 8) NOT NULL,
+    quantity Float64 NOT NULL,
+    trade_time DateTime64(3) NOT NULL,
+    is_market_maker Bool NOT NULL
+) ENGINE = MergeTree()
+ORDER BY (symbol, trade_time, trade_id)
